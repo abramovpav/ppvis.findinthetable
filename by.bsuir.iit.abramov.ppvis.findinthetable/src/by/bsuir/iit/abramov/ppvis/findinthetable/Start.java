84d3d96e87c93bfc69bf487522ddad58ec5b4e5b
abramovpav <abramovpav@gmail.com>
2013-04-23 08:36:48 -0430

    первый комит

---
+++ by.bsuir.iit.abramov.ppvis.findinthetable/src/by/bsuir/iit/abramov/ppvis/findinthetable/Start.java
@@ -0,0 +1,14 @@
+package by.bsuir.iit.abramov.ppvis.findinthetable;
+
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
